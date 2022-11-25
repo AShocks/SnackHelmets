1f2c67226ecef991f6f4700195d9ded97e57cea8
AShocks <oshaughnessy.adrian@gmail.com>
2022-11-25 01:03:37 +0000

    deployment commit

--- helmet_store/settings.py
+++ helmet_store/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["snackhelmets.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['snackhelmets.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'helmet_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
