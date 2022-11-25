47d98f0cbe1e3dd03ccfbda58f347d8419675460
AShocks <oshaughnessy.adrian@gmail.com>
2022-11-25 14:44:25 +0000

    Correct errors in allauth setup and create new superuser

--- helmet_store/settings.py
+++ helmet_store/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'helmet_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
